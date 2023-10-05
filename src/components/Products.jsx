65dc2e526dbe67fd764f8510d642269bf7019296
Cheemma1 <chidimma9521@gmail.com>
2023-10-05 10:23:58 +0100

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,45 +4,54 @@ import { Link } from "react-router-dom";
 
 const Products = () => {
   const [product, setProduct] = useState([]);
-  const [search, setSearch] = useState("")
-  useEffect(()=>{
-  const getproduct = async () => {
-    try {
-      const res = await axios.get("https://fakestoreapi.com/products");
-      setProduct(res.data);
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    const getproduct = async () => {
+      try {
+        const res = await axios.get("https://fakestoreapi.com/products");
+        setProduct(res.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
 
     getproduct();
-},[])
-console.log(product)
-  return(
-  
-  <div className=" my-10 bg-slate-50 p-3">
-<h2 className="text-center text-2xl py-3 font-bold">Our Product</h2>
-<input type="text"  placeholder="search product" value={search} onChange={(e)=>setSearch(e.target.value)} className="w-full outline-none rounded-md p-4 shadow-sm "/>
-<div className="grid grid-cols-4 gap-8 p-4 w-full">
-  { product.filter((item) => item.title.toLowerCase().includes (search.toLowerCase()))
-  .map((items)=>(
-    <Link to={`/details/${items?.id}`}
-    key={items?.id} 
-    >
-<div 
-className="  bg-yellow-100 p-2 ">
-  {/* <div className="w-full h-fit"> */}
-      <img src={items?.image} alt="product-image"  className="w-full h-full object-contain"/>
-  {/* </div> */}
-  
-    <h2 className="font-bold">{items?.title}</h2>
-    <p>${items?.price}</p>
-</div></Link>
-  ))}  
-</div>
-  </div>)
+  }, []);
+  console.log(product);
+  return (
+    <div className=" my-10 bg-slate-50 p-3">
+      <h2 className="text-center text-2xl py-3 font-bold">Our Product</h2>
+      <input
+        type="text"
+        placeholder="search product"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="w-full outline-none rounded-md p-4 shadow-sm "
+      />
+      <div className="grid grid-cols-4 gap-8 p-4 w-full">
+        {product
+          .filter((item) =>
+            item.title.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((items) => (
+            <Link to={`/details/${items?.id}`} key={items?.id}>
+              <div className="   ">
+                <div className="w-1/2 h-full">
+                  <img
+                    src={items?.image}
+                    alt="product-image"
+                    className="w-1/2  h-1/2 object-contain"
+                  />
+                </div>
 
-
+                <h2 className="font-bold">{items?.title}</h2>
+                <p>${items?.price}</p>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-
-    fontFamily:{
-      poppins:['Poppins' ,'sans-serif']
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
     },
     extend: {
-      backgroundImage:{
-        'hero-pattern': "url('./src/assets/woman-holding-various-shopping-bags-copy-space.jpg')",
+      backgroundImage: {
+        "hero-pattern":
+          "url('./src/assets/woman-holding-various-shopping-bags-copy-space.jpg')",
 
-        'footer-pattern': "url('./src/assets/woman-holding-various-shopping-bags-copy-space.jpg')",
-
-      }
+        "footer-pattern":
+          "url('./src/assets/woman-holding-various-shopping-bags-copy-space.jpg')",
+      },
     },
   },
   plugins: [],
-}
-
- 
+};
